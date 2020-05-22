7be670f0e3bd195bd70730261d12fad6a1cc94be
Md Alamin Bhuian <alamin.cse.pu@gmail.com>
2020-05-22 18:13:38 +0600

    Initial Commit

---
+++ src/components/TopBanner/TopBanner.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Container} from "react-bootstrap";
+
+class TopBanner extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container>
+
+                </Container>
+
+            </Fragment>
+        );
+    }
+}
+
+export default TopBanner;
