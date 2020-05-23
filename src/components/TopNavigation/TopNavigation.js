6cd1e335616704771404ac2d54d9924c13c72146
Md Alamin Bhuian <alamin.cse.pu@gmail.com>
2020-05-23 20:37:21 +0600

    Navbar Complete

--- src/components/TopNavigation/TopNavigation.js
+++ src/components/TopNavigation/TopNavigation.js
@@ -1,25 +1,36 @@
 import React, {Component, Fragment} from 'react';
 import {Navbar, Nav} from "react-bootstrap";
 import "../../asset/css/custom.css"
+import profile_logo from "../../asset/image/profile_logo.png"
+import profile_logo2 from "../../asset/image/profile_logo2.webp"
 
 class TopNavigation extends Component {
 
     constructor(){
         super();
         this.state={
-            navBarTitle:"navTitle"
+            navBarTitle:"navTitle",
+            navBarProfile: [profile_logo],
+            navBarBackground:"navBackGround",
+            navBarItem:"navItem",
         }
     }
 
     onScroll=()=>{
         if(window.scrollY>100){
             this.setState({
-                navBarTitle:"navTitleScroll"
+                navBarTitle:"navTitleScroll",
+                navBarProfile: [profile_logo2],
+                navBarBackground:"navBackGroundScroll",
+                navBarItem:"navItemScroll",
             })
         }
         else if(window.scrollY<100){
             this.setState({
-                navBarTitle:"navTitle"
+                navBarTitle:"navTitle",
+                navBarProfile: [profile_logo],
+                navBarBackground:"navBackGround",
+                navBarItem:"navItem",
             })
         }
     }
@@ -30,20 +41,20 @@ class TopNavigation extends Component {
     render() {
         return (
             <Fragment>
-                <Navbar fixed={"top"} collapseOnSelect expand="lg" bg="dark" variant="dark">
-                    <Navbar.Brand className={this.state.navBarTitle} href="#home">Alamin Bhuiyan</Navbar.Brand>
+                <Navbar fixed={"top"} className={this.state.navBarBackground} collapseOnSelect expand="lg" variant="dark">
+                    <Navbar.Brand className={this.state.navBarTitle} href="#home"><img height={30} src={this.state.navBarProfile}/> Alamin Bhuiyan</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
 
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">HOME</Nav.Link>
-                            <Nav.Link href="#deets">SERVICES</Nav.Link>
-                            <Nav.Link href="#deets">COURSES</Nav.Link>
-                            <Nav.Link href="#deets">PORTFOLIO</Nav.Link>
-                            <Nav.Link href="#deets">CONTACT</Nav.Link>
-                            <Nav.Link href="#deets">ABOUT</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">SERVICES</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">COURSES</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">PORTFOLIO</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">CONTACT</Nav.Link>
+                            <Nav.Link className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
