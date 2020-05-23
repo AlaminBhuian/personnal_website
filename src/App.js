54bbd725bfc4a683c850fa2da0a6f91e0bc2fb1d
Md Alamin Bhuian <alamin.cse.pu@gmail.com>
2020-05-23 18:02:58 +0600

    Navbar add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
 import TopBanner from "./components/TopBanner/TopBanner";
 import Services from "./components/Services/Services";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
 
 function App() {
   return (
     <Fragment>
+        <TopNavigation></TopNavigation>
       <TopBanner></TopBanner>
         <Services></Services>
 
